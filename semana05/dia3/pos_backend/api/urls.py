c3d00d6ed698c649383b0be43baa87b67a093f7d
FherAmableRicse <amablericsefher@gmail.com>
2022-07-15 23:14:02 -0500

    update urls py

--- semana05/dia3/pos_backend/api/urls.py
+++ semana05/dia3/pos_backend/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.IndexView.as_view()),
-    path('categoria/',views.CategoriaView.as_view()),
+    path('categoria',views.CategoriaView.as_view()),
     path('mesa',views.MesaView.as_view()),
     path('plato',views.PlatoView.as_view()),
     path('categoria/<int:categoria_id>/',views.CategoriaDetail.as_view()),
