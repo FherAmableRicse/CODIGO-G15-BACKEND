fd75a6ca703bb9ba92feb5efe18aab3e48babbc7
FherAmableRicse <amablericsefher@gmail.com>
2022-08-16 23:01:32 -0500

    frontend && backend (upload image cloudinary) - mongoose && mysql

---
+++ semana09/dia4_5/backend/src/routes/producto.routes.js
@@ -0,0 +1,12 @@
+const {Router} = require('express');
+const router = Router();
+const {getAll,create,uploadImage} = require('../controllers/producto.controllers');
+
+router.route('/')
+       .get(getAll)
+       .post(create)
+
+router.route('/upload')
+       .post(uploadImage)
+
+module.exports = router;
