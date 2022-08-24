1a62f0dc0730f25fdd09e74f69e9a57b080aeae4
FherAmableRicse <amablericsefher@gmail.com>
2022-08-23 19:16:37 -0500

    node js and django api cursos y api miembros

---
+++ semana10/dia5/perudemy/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Catalogo from './modulos/cursos/components/Catalogo';
+import Cursos from './modulos/cursos/pages/Cursos';
+
+const App = ()=>{
+
+  return(
+    <>  
+      <Cursos/>
+
+    </>
+    
+    )
+}
+export default App;
